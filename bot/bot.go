1df449d8c91d3f26bd1732f3f55c749ce6be864d
egorikas <egorgrishechko@gmail.com>
2019-04-06 18:14:15 +0300

    coordFromPointAndAngle

--- bot/bot.go
+++ bot/bot.go
@@ -248,24 +248,24 @@ func angle(from client.Cell, to client.Cell) string {
 	panic("smth broken")
 }
 
-func coordFromPointAndAngle(from client.Cell, heading string) client.Cell {
+func (b *Bot) coordFromPointAndAngle(from client.Cell, heading string) client.Cell {
 	if heading == NorthEast {
-		return client.Cell{X: from.X + 1, Y: from.Y, Z: from.Z - 1}
+		return b.cellsIndex[from.X+1][from.Y][from.Z-1]
 	}
 	if heading == NorthWest {
-		return client.Cell{X: from.X, Y: from.Y + 1, Z: from.Z - 1}
+		return b.cellsIndex[from.X][from.Y+1][from.Z-1]
 	}
 	if heading == West {
-		return client.Cell{X: from.X - 1, Y: from.Y + 1, Z: from.Z}
+		return b.cellsIndex[from.X-1][from.Y+1][from.Z]
 	}
 	if heading == SouthWest {
-		return client.Cell{X: from.X - 1, Y: from.Y, Z: from.Z + 1}
+		return b.cellsIndex[from.X-1][from.Y][from.Z+1]
 	}
 	if heading == SouthEast {
-		return client.Cell{X: from.X, Y: from.Y - 1, Z: from.Z + 1}
+		return b.cellsIndex[from.X][from.Y-1][from.Z+1]
 	}
 	if heading == East {
-		return client.Cell{X: from.X + 1, Y: from.Y - 1, Z: from.Z}
+		return b.cellsIndex[from.X+1][from.Y-1][from.Z]
 	}
 
 	panic("smth broken")
